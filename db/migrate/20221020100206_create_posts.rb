e02f287cae1ffb964fbe2c5815d45c9b53f5e6cd
Kinyua <alfkinyua@gmail.com>
2022-10-20 16:58:21 +0300

    add user model method that returns the 3 most recent posts for a given user.

--- db/migrate/20221020100206_create_posts.rb
+++ db/migrate/20221020100206_create_posts.rb
@@ -1,4 +1,4 @@
-class CreatePost < ActiveRecord::Migration[7.0]
+class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
@@ -6,7 +6,7 @@ class CreatePost < ActiveRecord::Migration[7.0]
       t.integer :comments_counter
       t.integer :likes_counter
 
-      t.timestamps
+        t.timestamps
     end
   end
 end
