c0a5f54a47fed53ebb801fb0d20aa5633e55b82b
Kinyua <alfkinyua@gmail.com>
2022-10-21 18:50:42 +0300

    fix linters error

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -2,13 +2,10 @@ require 'rails_helper'
 require_relative '../app/models/like'
 
 describe Like, type: :model do
-    subject { Like.new(id: 10, created_at: '2022-10-20', updated_at: '2022-10-20',post_id:4,author_id:2) }
-  
-    it 'author id should be an int' do
-        subject.post_id = 6
-        expect(subject).to_not be_valid
-      end
-  
- 
-  
- end
+  subject { Like.new(id: 10, created_at: '2022-10-20', updated_at: '2022-10-20', post_id: 4, author_id: 2) }
+
+  it 'author id should be an int' do
+    subject.post_id = 6
+    expect(subject).to_not be_valid
+  end
+end
