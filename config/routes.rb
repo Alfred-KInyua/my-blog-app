505507dd439b50984ff4146609cb5d939bea6cd4
Kinyua <alfkinyua@gmail.com>
2022-10-24 18:39:40 +0300

    Add posts view files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   get 'users', to:'users#index'
   get 'users/:user_id', to:'users#show'
-  get 'posts', to:'posts#index'
-  get 'posts/:posts_id', to: 'posts#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
   # get /users to controler to action 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
