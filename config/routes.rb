efefde252ddf2dfd9e5a92489d364a1a9194241c
Kinyua <alfkinyua@gmail.com>
2022-10-24 18:28:47 +0300

    Add users routes, action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users', to:'users#index'
+  get 'users/:user_id', to:'users#show'
+  # get /users to controler to action 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
